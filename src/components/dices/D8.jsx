c24fd60c34ec0cea773a0f39ea991a6722ecfdf0
bisk8s <bisk8scode@gmail.com>
2021-03-24 12:17:19 -0300

    d8 uv mapping

--- src/components/dices/D8.jsx
+++ src/components/dices/D8.jsx
@@ -20,6 +20,23 @@ export default function D8() {
     };
   });
 
+  geometry.faces.forEach((face, i, arr) => {
+    let materialIndex = i;
+
+    console.log({ i, materialIndex });
+    face.materialIndex = materialIndex;
+  });
+
+  const uvMapping = (uv, i) => {
+    uv[0].set(1.3, 0);
+    uv[1].set(0.33, 1.3);
+    uv[2].set(0, 0);
+  };
+
+  // uv mapping
+  geometry.faceVertexUvs[0].forEach(uvMapping);
+  geometry.uvsNeedUpdate = true;
+
   return (
     <mesh ref={ref} geometry={geometry} material={D20Materials.slice(1)} />
   );
